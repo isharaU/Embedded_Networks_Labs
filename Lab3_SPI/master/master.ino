a37a00831b48756c620ff82f044f829659640e63
isharaU <wellahewawiud.20@gmail.com>
2025-02-20 13:10:48 +0530

    Refactor SPI master code for improved data handling and debugging

--- Lab3_SPI/master/master.ino
+++ Lab3_SPI/master/master.ino
@@ -1,18 +1,35 @@
 #include <SPI.h>
 
-#define SS_PIN 10  
-
 void setup() {
-    pinMode(SS_PIN, OUTPUT);
-    SPI.begin();  
-    Serial.begin(9600);
+  // Initialize SPI as Master
+  SPI.begin();
+  
+  // Set SS pin as output
+  pinMode(SS, OUTPUT);
+  
+  // Initialize Serial for debugging
+  Serial.begin(9600);
 }
 
 void loop() {
-    digitalWrite(SS_PIN, LOW);  // Enable slave
-    char receivedData = SPI.transfer('A');  // Send 'A' and receive response
-    Serial.print("Master received: ");
-    Serial.println(receivedData);
-    digitalWrite(SS_PIN, HIGH); // Disable slave
-    delay(1000);
+  // Data to be sent to the slave (decimal number)
+  byte dataToSend = 123;  // Example data (123 in decimal)
+  
+  // Select the slave by setting SS low
+  digitalWrite(SS, LOW);
+  
+  // Send data to the slave
+  byte receivedData = SPI.transfer(dataToSend);
+  
+  // Deselect the slave by setting SS high
+  digitalWrite(SS, HIGH);
+  
+  // Print the sent and received data in decimal
+  Serial.print("Sent: ");
+  Serial.print(dataToSend);
+  Serial.print(" | Received: ");
+  Serial.println(receivedData);
+  
+  // Wait for a second before sending the next data
+  delay(1000);
 }
